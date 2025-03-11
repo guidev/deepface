5719ec3b1ac039901c1d73d98dea51f3c36edd0d
Guglielmo Faglioni <guglielmo@gorillatech.io>
2025-03-11 21:19:04 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 echo "Starting the application..."
 exec "$@"
 
-gunicorn --workers=1 --timeout=7200 --bind=0.0.0.0:5000 --log-level=debug --access-logformat='%(h)s - - [%(t)s] "%(r)s" %(s)s %(b)s %(L)s' --access-logfile=- "app:create_app()"
+gunicorn --workers=1 --timeout=7200 --bind=0.0.0.0:8000 --log-level=debug --access-logformat='%(h)s - - [%(t)s] "%(r)s" %(s)s %(b)s %(L)s' --access-logfile=- "app:create_app()"
